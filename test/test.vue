d8ff0c69ed91485460a7029fd5399a47a4d8b29a
inlinger <inlinger@outlook.com>
2022-07-12 14:22:56 +0800

    init

---
+++ test/test.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Test',
+
+  data: () => ({
+    test: 'value',
+  }),
+}
+</script>
+
+<template>
+  <div>
+    <span>123</span>
+    <span>{{ test }}</span>
+  </div>
+</template>
+
+<style>
+</style>
