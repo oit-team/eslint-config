ccf5b794bb339552e0ad010a10bb312e14f10f35
inlinger <inlinger@outlook.com>
2022-07-18 11:45:59 +0800

    fix: update vue/custom-event-name-casing kebab-case

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -57,7 +57,7 @@ module.exports = {
     ],
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'vue/component-options-name-casing': ['error', 'PascalCase'],
-    'vue/custom-event-name-casing': ['error', 'camelCase'],
+    'vue/custom-event-name-casing': ['error', 'kebab-case'],
     'vue/define-macros-order': [
       'error', {
         order: ['defineProps', 'defineEmits'],
